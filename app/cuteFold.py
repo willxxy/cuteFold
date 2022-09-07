47c2921e4bb37277de13d55dd37731a1e4ee7bc0
hamesjan <jhan31903@gmail.com>
2022-09-06 18:22:07 -0700

    made main file

--- app/cuteFold.py
+++ app/cuteFold.py
@@ -3,10 +3,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import py3Dmol
-# 1A2C
-# Structure of thrombin inhibited by AERUGINOSIN298-A from a BLUE-GREEN ALGA
 
-st.write("cuteFold")
+
+st.title("cuteFold")
 xyzview = py3Dmol.view(query='pdb:1A2C')
 xyzview.setStyle({'cartoon': {'color': 'spectrum'}})
 showmol(xyzview, height=500, width=800)
