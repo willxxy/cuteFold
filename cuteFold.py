651c65776b169936e4fb236fcec0ec148e5e3956
William Han <willhan327@outlook.com>
2022-09-07 03:11:27 -0700

    initial update

---
+++ cuteFold.py
@@ -0,0 +1,7 @@
+from Bio.PDB import *
+import nglview as nv
+import ipywidgets
+
+
+if __name__ == '__main__':
+    
--- sample.py
+++ sample.py
@@ -27,6 +27,5 @@ st.bar_chart(hist_values)
 # Some number in the range 0-23
 hour_to_filter = st.slider('hour', 0, 23, 17)
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
 st.subheader('Map of all pickups at %s:00' % hour_to_filter)
 st.map(filtered_data)
